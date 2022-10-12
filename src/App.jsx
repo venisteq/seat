b603b0f89c9ebfaf8cd3ff15864c69821f47b036
Aman kumar <32014475+Dev-Aman7@users.noreply.github.com>
2022-10-12 14:32:27 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+const App = () => {
+    return (
+        <Typography variant="h1">
+            A template for a react app using material-ui
+        </Typography>
+    );
+};
+
+export default App;
