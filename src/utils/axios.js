7a96f3d8f522b5397d089d19ccee4a3160c3d450
venisteq <115787296+venisteq@users.noreply.github.com>
2022-10-14 17:55:35 +0530

    add hosted url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BaseClient = axios.create({
-    baseURL: "http://localhost:9001",
+    baseURL: "https://seat-backend.herokuapp.com/",
 });
 
 export default BaseClient;
